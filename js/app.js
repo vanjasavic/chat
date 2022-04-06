5ae00045395ef82a59d657073998dc12c75b4ae6
Mazikis <savic.vanja17@gmail.com>
2022-04-06 15:36:11 +0200

    0.8.5

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
 const body = document.getElementById('chat__body');
+const main = document.getElementById('main');
 let button = document.getElementById('chat__button');
 let input = document.getElementById('chat__input');
 
+function validate () {
+
+    const dangerAlert = document.createElement("div");
+    dangerAlert.className += 'chat__alert chat__alert__danger';
+    dangerAlert.innerHTML = "Unesi poruku"
+
+    if (msg === '') {
+        main.prepend(dangerAlert);
+        parent.removeChild(chtmsg);
+    } 
+
+}
+
 button.addEventListener("click", function() {
     msg = input.value;
-    let chtmsg = document.createElement("p");
-    chtmsg.className += 'chat__message chat__message__primary';
-    chtmsg.append(msg);
-    body.append(chtmsg);
+    validate()
+    let chtMsg = document.createElement("p");
+    chtMsg.className += 'chat__message chat__message__primary';
+    chtMsg.append(msg);
+    body.append(chtMsg);
+    input.value = "";
 });
+
